69ee38d8846dceb06cdac837c5d711c6517c2700
Ashwani@27155 <67748196+ashwanikumarkushwaha@users.noreply.github.com>
2022-02-24 14:14:12 +0530

    Create four_sum.java

---
+++ four_sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Set <List<Integer>> set=new HashSet<>();
+        if(nums.length==0){
+            return new ArrayList<>(set);
+        }
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-3;i++){
+            for(int j=i+1;j<nums.length-2;j++){
+                int k=j+1;
+                int l=nums.length-1;
+                while(k<l){
+                    int sum=nums[i]+nums[j]+nums[k]+nums[l];
+                    if(sum==target){
+                        set.add(Arrays.asList(nums[i],nums[j],nums[k++],nums[l--]));
+                    }
+                    else if(sum>target){
+                        l--;
+                    }
+                    else if(sum<target){
+                        k++;
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
+}
